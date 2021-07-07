469f4f1a7b0f11d9ee628378bc00c350f2b1e6e2
hehe0618 <806028706@qq.com>
2021-07-07 23:33:31 +0800

    课程01

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,37 @@
 // pages/home/home.js
+// getApp()获取App()产生的示例对象
+const app = getApp()
+console.log(app.globaData.name);
+console.log(app.globaData.age);
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Coderwhy',
+    age: 18,
+    students: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 110, name: 'james', age: 30},
+      {id: 110, name: 'curry', age: 30},
+      {id: 110, name: 'why', age: 30}
+    ],
+    coundet: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  addClick() {
+    // 1.错误方法 和vue不一样
+    // this.data.coundet += 1;
+    // console.log(this.data.coundet);
+    // 2.正确方法使用 setData()
+    this.setData({
+      coundet: this.data.coundet + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  reClick() {
+    this.setData({
+      coundet: this.data.coundet - 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取用户信息
+  GetUserInfoClick(event) {
+    console.log(event);
   }
 })
